554894a942fbaa3cc6d477c71268dd3cae0ee239
totNate <111439441+totNate@users.noreply.github.com>
2024-02-24 10:09:29 -0500

    auton finalized and downloaded onto Error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Diagnostics.java
@@ -8,38 +8,11 @@ import org.firstinspires.ftc.teamcode.drive.subsystems.CENTERSTAGE_Bot;
 
 @TeleOp
 public class Diagnostics extends OpMode {
-//    private DcMotor fr;
-//    private DcMotor fl;
-//    private DcMotor br;
-//    private DcMotor bl;
-//    private DistanceSensor rightDistance;
-//    private DistanceSensor leftDistance;
-//    private DistanceSensor centerDistance;
-
     private CENTERSTAGE_Bot bot;
 
     @Override
     public void init() {
-
         bot = new CENTERSTAGE_Bot(this);
-
-
-//        fr = hardwareMap.get(DcMotor.class, "fr");
-//        fl = hardwareMap.get(DcMotor.class, "fl");
-//        br = hardwareMap.get(DcMotor.class, "br");
-//        bl = hardwareMap.get(DcMotor.class, "bl");
-//        rightDistance = hardwareMap.get(DistanceSensor.class, "rightSensor");
-//        leftDistance = hardwareMap.get(DistanceSensor.class, "leftSensor");
-//        centerDistance = hardwareMap.get(DistanceSensor.class, "centerSensor");
-//
-//        fl.setDirection(DcMotorSimple.Direction.REVERSE);
-//        bl.setDirection(DcMotorSimple.Direction.REVERSE);
-//
-//        imu = hardwareMap.get(IMU.class, "imu");
-//        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-//                DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
-//        imu.initialize(parameters);
-
     }
 
 
